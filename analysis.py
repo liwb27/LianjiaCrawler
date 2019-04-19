c9aa8a217b78e8867e2aadef9d0e7118276ca17e
liwb <bolia@yeah.net>
2019-04-19 22:40:35 +0800

    更新数据分析文件

--- analysis.py
+++ analysis.py
@@ -7,8 +7,13 @@ class Analysis(object):
     client = MongoClient('mongodb://localhost:27017')
     db = client.lianjia
     collection = db.house_detail
+    collection_price = db.house_price
+    
 
-    def most_house_in_community(self):  # 房屋数量最多的小区
+    def most_house_in_community(self):
+        '''
+        # 房屋数量最多的小区
+        '''
         result = self.collection.aggregate([
             {
                 '$group': {
@@ -65,74 +70,147 @@ class Analysis(object):
                 }
             }
         ])
+        print('房屋面积比例')
+        for i in result:
+            print(i)
+ 
+    # 小区均价
+    def group_by_community(self, date=datetime.strptime(str(date.today()), '%Y-%m-%d')):
+        result = self.collection_price.aggregate([
+            {
+                '$match': {
+                    'date': date,
+                }
+            },
+            {
+                '$group': {
+                    '_id': "$house_id",
+                    '单价': { '$avg': "$单价" },
+                }
+            },
+            {
+                '$lookup':
+                    {
+                        'from': 'house_detail',
+                        'localField': '_id',
+                        'foreignField': '_id',
+                        'as': 'detail'
+                    }
+            },
+            {
+                '$group': {
+                    '_id': "$detail.小区名称",
+                    'count': { '$sum': 1 },
+                    'avg': { '$avg': "$单价" }
+                }
+            },
+            { "$match": {"count": { '$gte': 20} }},
+            # { "$match": {"_id": "建业"}},
+            { "$sort": { "avg": -1 } },
+        ])
         for i in result:
             print(i)
 
-    # def top_community(self):  # 均价最高小区
-    #     result = self.collection.aggregate([
-    #         {
-    #             "$unwind": "$价格"
-    #         },
-    #         {
-    #             "$match": {
-    #                 "价格.date": datetime.strptime(str(date.today()), '%Y-%m-%d')
-    #             }
-    #         },
-    #         {
-    #             '$group': {
-    #                 '_id': "$小区名称",
-    #                 'count': {'$sum': 1},
-    #                 'avg': {'$avg': "$价格.单价"}
-    #             }
-    #         },
-    #         {"$sort": {"avg": -1}},
-    #         {"$limit": 20}
-    #     ])
-    #     for i in result:
-    #         print(i)
+    def rise_and_fall(self, startDate = datetime.strptime('2019-01-01', '%Y-%m-%d'), endDate = datetime.strptime(str(date.today()), '%Y-%m-%d')):
+        '''
+        涨跌数据
+        '''
+        group_price = self.collection_price.aggregate([
+            {
+                "$match": {
+                    '$or': [
+                        {'date': startDate},
+                        {'date': endDate}
+                    ]
+                }
+            },
+            { "$sort": {'date':-1} },
+            {
+                '$group': {
+                    '_id': {
+                        "house_id": "$house_id"
+                    },
+                    'count': {'$sum': 1},
+                    'data': {'$push': {
+                        "price":"$单价",
+                        "date":"$date"
+                    }}
+                }
+            },
+            {
+                "$match": {
+                    "count": 2
+                }
+            },
+            {
+                "$project": {
+                    'end':  { '$arrayElemAt': ["$data.price", 0] },
+                    'start': { '$arrayElemAt': ["$data.price", -1] },
+                }
+            },
+            {
+                "$project": {
+                    "isRising" : {
+                        "$cond" : {
+                            "if": { '$gt': [ "$start", "$end" ] },
+                            "then": -1,
+                            "else": {
+                                "$cond": {
+                                    "if": {'$eq': [ "$start", "$end" ]},
+                                    "then": 0,
+                                    "else": 1
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "rising":"$isRising",
+                    },
+                    "count": {"$sum":1}
+                }
+            }
+        ])
+        for item in group_price:
+            print(item)
 
-    # def rise_and_fale(self):  # 涨跌数据
-    #     zhang = 0
-    #     die = 0
-    #     zhang_max = -1
-    #     die_max = -1
-    #     for item in self.collection.find():
-    #         price = item['价格']
-    #         diff = price[0]['总价'] - price[-1]['总价']
-    #         if diff != 0:
-    #             if diff < 0:
-    #                 zhang += 1
-    #                 if -diff > zhang_max:
-    #                     zhang_max = -diff
-    #                     zhang_max_id = item['_id']
-    #                 print('+++', item['_id'])
-    #             else:
-    #                 die += 1
-    #                 if diff > die_max:
-    #                     die_max = diff
-    #                     die_max_id = item['_id']
-    #                 print('---', item['_id'])
-    #     print('涨:{0}, 跌:{1}'.format(zhang, die))
-    #     print('涨max:{0}--{1}, 跌max:{2}--{3}'.format(zhang_max,
-    #                                                 zhang_max_id, die_max, die_max_id))
+    def avg_orderby_price(self):  # 日期均价，价格排序
+        result = self.collection_price.aggregate([
+            {
+                '$group': {
+                    '_id': {
+                        "日期": "$date"
+                    },
+                    'count': {'$sum': 1},
+                    'avg': {'$avg': "$单价"}
+                }
+            },
+            {"$sort": {"avg": -1}},
+        ])
+        for i in result:
+            print(i)
 
-    # def avg_orderby_date(self):  # 日期均价
-    #     result = self.collection.aggregate([
-    #         {
-    #             "$unwind": "$价格"
-    #         },
-    #         {
-    #             '$group': {
-    #                 '_id': "$价格.date",
-    #                 'count': {'$sum': 1},
-    #                 'avg': {'$avg': "$价格.单价"}
-    #             }
-    #         },
-    #     ])
-    #     data = [i for i in result if i['count'] > 40000]
-    #     data.sort(key=lambda param: param['_id'])
-    #     for i in data:
-    #         print(i)
+    def avg_orderby_date(self):  # 日期均价，日期排序
+        result = self.collection_price.aggregate([
+            {
+                '$group': {
+                    '_id': {
+                        "日期": "$date"
+                    },
+                    'count': {'$sum': 1},
+                    'avg': {'$avg': "$单价"}
+                }
+            },
+            {"$sort": {"_id.日期": -1}},
+        ])
+        for i in result:
+            print(i)
 
 if __name__ == '__main__':
+    Analysis().group_by_community(date=datetime.strptime('2019-01-28', '%Y-%m-%d'))
+    Analysis().rise_and_fall(startDate=datetime.strptime('2019-01-28', '%Y-%m-%d'), endDate=datetime.strptime('2019-03-01', '%Y-%m-%d'))
+    Analysis().avg_orderby_price()
     Analysis().group_by_aera()
--- scripts.js
+++ scripts.js
@@ -44,77 +44,55 @@ db.house_detail.aggregate([
         }
     }
 ])
-// 区域均价
-db.house_detail.explain('executionStats').aggregate([
+
+//两表合并
+db.house_detail.aggregate([    
     {
-        "$unwind": "$价格"
+        $lookup:
+            {
+                from: 'house_price',
+                localField: '_id',
+                foreignField: 'house_id',
+                as: 'price_data'
+            }
     },
+])
+
+// 区域均价
+// db.house_detail.explain('executionStats').aggregate([
+db.house_price.aggregate([
     {
-        "$match": {
-            "价格.date": new Date("2019-01-02")
+        $match: {
+            'date': new Date("2019-03-01"),
         }
     },
     {
         $group: {
-            _id: "$小区名称",
-            count: { $sum: 1 },
-            avg: { $avg: "$价格.单价" }
+            _id: "$house_id",
+            '单价': { $avg: "$单价" },
         }
     },
-    { "$sort": { "count": -1 } },
+    {
+        $lookup:
+            {
+                from: 'house_detail',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'detail'
+            }
+    },
+    {
+        $group: {
+            _id: "$detail.小区名称",
+            count: { $sum: 1 },
+            avg: { $avg: "$单价" }
+        }
+    },
+    { "$match": {"count": { $gte: 70} }},
+    { "$sort": { "avg": -1 } },
 ]);
 
 //按日期均值
-db.house_detail.aggregate([
-    {
-        "$unwind": "$价格"
-    },
-    {
-        $group: {
-            _id: "$价格.date",
-            count: { $sum: 1 },
-            avg: { $avg: "$价格.单价" }
-        }
-    },
-    { "$sort": { "_id": -1 } },
-]);
-
-
-db.house_detail.aggregate([
-    {
-        "$unwind": "$价格"
-    },
-    {
-        $group: {
-            _id: {
-                "区域":"$小区名称",
-                "日期":"$价格.date"
-            },
-            count: { $sum: 1 },
-            avg: { $avg: "$价格.单价" }
-        }
-    },
-    { "$sort": { "avg": -1 } },
-]);
-
-
-db.house_detail.explain().aggregate([
-    {
-        "$unwind": "$价格"
-    },
-    {
-        $group: {
-            _id: {
-                "区域":"$小区名称",
-                "日期":"$价格.date"
-            },
-            count: { $sum: 1 },
-            avg: { $avg: "$价格.单价" }
-        }
-    },
-    { "$sort": { "avg": -1 } },
-]);
-
 db.house_price.aggregate([
     {
         $group: {
@@ -140,3 +118,58 @@ db.house_price.aggregate([
     },
     { "$sort": { "_id": -1 } },
 ]);
+
+// 涨跌统计
+db.house_price.aggregate([
+    {
+        "$match": {
+            '$or': [
+                {'date': new Date("2019-01-01")},
+                {'date': new Date("2019-03-01")}
+            ]
+        }
+    },
+    { "$sort": {'date':-1} },
+    {
+        '$group': {
+            '_id': {
+                "house_id": "$house_id"
+            },
+            'count': {'$sum': 1},
+            'data': {'$push': {
+                "price":"$单价",
+                "date":"$date"
+            }}
+        }
+    },
+    {
+        "$match": {
+            "count": 2
+        }
+    },
+    {
+        "$project": {
+            'end':  { $arrayElemAt: ["$data.price", 0] },
+            'start': { $arrayElemAt: ["$data.price", -1] },
+        }
+    },
+    {
+        "$project": {
+            "isRising" : {
+                "$cond" : {
+                    "if": { $gt: [ "$start", "$end" ] },
+                    "then": false,
+                    "else": true
+                }
+            }
+        }
+    },
+    {
+        "$group": {
+            "_id": {
+                "rising":"$isRising",
+            },
+            "count": {"$sum":1}
+        }
+    }
+])
